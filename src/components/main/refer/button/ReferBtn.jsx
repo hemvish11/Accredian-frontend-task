39414692a4fb7b4e0d143e83b44460d9169fe05e
hemant <hemvish11@gmail.com>
2024-07-08 02:10:38 +0530

    more styling added to faqs section

--- src/components/main/refer/button/ReferBtn.jsx
+++ src/components/main/refer/button/ReferBtn.jsx
@@ -10,7 +10,7 @@ const ReferBtn = () => {
         dispatch(setIsReferralBoxOpened(!isReferralBoxOpened));
     }
     return (
-        <button onClick={() => handleClose()} className="bg-[#1A73E8] text-white px-8 py-2 rounded-md text-[15px]/[24px] lg:text-[16px]/[24px] 3xl:text-[20px]/[24px]">Refer Now</button>
+        <button onClick={() => handleClose()} className="bg-[#1A73E8] hover:bg-blue-600 hover:scale-[1.02] text-white px-8 py-2 rounded-md text-[15px]/[24px] lg:text-[16px]/[24px] 3xl:text-[20px]/[24px]">Refer Now</button>
     )
 }
 
